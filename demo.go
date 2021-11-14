f626612c703e8aeed1143201801b586c34d62ddc
DeadFish <deadfish@DeadFishdeMacBook-Pro.local>
2021-11-14 14:30:52 +0800

    update

--- demo.go
+++ demo.go
@@ -26,7 +26,7 @@ func main() {
 	// 创建虚拟机
 	levm.CreateEVM()
 
-	// 1 部署合约到state上
+	// 1. 部署合约到state上
 	contractAddress, _, err := levm.Deploy(caller, contractBinFile, levm.EVMConfig.GasLimit, levm.EVMConfig.Value)
 	if err != nil {
 		fmt.Println(err)
@@ -36,7 +36,7 @@ func main() {
 	// 获取调用的函数地址。函数名和参数名组成的字符串，哈希后的8个位
 	input := crypto.Keccak256([]byte("get()"))[0:4]
 
-	// 2 调用合约
+	// 2. 调用合约
 	ret, _, err := levm.Call(caller, contractAddress, input, levm.EVMConfig.GasLimit, levm.EVMConfig.Value)
 	if err != nil {
 		fmt.Println(err)
